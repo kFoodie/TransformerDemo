6342905cc5b127006dc7ef61b2d31ae69cf0b6e4
郑国凯 <>
2018-12-01 09:21:24 +0800

    Update infer.sh

--- transformer入门实战/train/infer.sh
+++ transformer入门实战/train/infer.sh
@@ -12,14 +12,15 @@ TMP_DIR=$DATA_DIR
 TRAIN_DIR=$HOME/t2t_train/$PROBLEM/$MODEL-$HPARAMS
 
 # Decode
-t2t-decoder --data_dir=./t2t_data
-    --problem=translate_enzh_wmt32k
-    --model=transformer
-    --hparams_set=transformer_base_single_gpu
-    --output_dir=./t2t_train/translate_enzh_wmt32k/transformer-transformer_base_single_gpu/
-    --decode_hparams="beam_size=4,alpha=0.6"
-    --decode_from_file=./t2t_data/wmt_enzh_32768k_tok_dev.lang1
-    --decode_to_file=translation.zh
+t2t-decoder --data_dir=./t2t_data \
+    --problem=translate_enzh_wmt32k \
+    --model=transformer \
+    --hparams_set=transformer_base_single_gpu \
+    --output_dir=./t2t_train/translate_enzh_wmt32k/transformer-transformer_base_single_gpu/ \
+    --decode_hparams="beam_size=4,alpha=0.6" \
+    --decode_from_file=./t2t_data/wmt_enzh_32768k_tok_dev.lang1 \
+    --decode_to_file=./t2t_data/translation.zh \
 
-t2t-bleu --translation=./t2t_data/translation.zh
-    --reference=./t2t_data/wmt_enzh_32768k_tok_dev.lang2
+t2t-bleu --translation=./t2t_data/translation.zh \
+    --reference=./t2t_data/wmt_enzh_32768k_tok_dev.lang2 \
+
